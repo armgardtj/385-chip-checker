fcbdf354320aff593e9c5e54ec206bfb9d7a0372
John <johnra2@illinois.edu>
2020-12-03 21:38:50 -0600

    Added 7404

--- chip_7404.sv
+++ chip_7404.sv
@@ -1,18 +1,18 @@
 module chip_7402( input logic Clk, 
 						input logic Reset,
 						input logic Run,
-						input logic Pin13,
-						output logic Pin12,
+						output logic Pin13,
+						input logic Pin12,
 						output logic Pin11,
 						input logic Pin10,
 						output logic Pin9,
-						output logic Pin8,
-						output logic Pin6,
+						input logic Pin8,
+						input logic Pin6,
 						output logic Pin5,
 						input logic Pin4,
 						output logic Pin3,
-						output logic Pin2,
-						input logic Pin1,
+						input logic Pin2,
+						output logic Pin1,
 						output logic Done,
 						output logic RSLT,
 						input logic DISP_RSLT);
@@ -26,7 +26,7 @@ enum logic [3:0] { Halted,
 reg [1:0] inputs;
 
 logic RSLT_Save;
-const int max_states = 3;
+const int max_states = 1;
 
 always_ff @ (posedge Clk)
 begin
@@ -82,24 +82,25 @@ always_latch
 			end
 			Test :
 			begin
-				A = inputs[1];
-				B = inputs[0];
-				Y = ~|inputs;
-				Pin2 = A;
-				Pin3 = B;
-				if (Pin1 != Y)
+				A = inputs[0];
+				Y = ~inputs;
+				Pin1 = A;
+				if (Pin2 != Y)
 					RSLT_Save = 0;
-				Pin5 = A;
-				Pin6 = B;
+				Pin3 = A;
 				if (Pin4 != Y)
 					RSLT_Save = 0;
-				Pin8 = A;
-				Pin9 = B;
-				if (Pin10 != Y)
+				Pin5 = A;
+				if (Pin6 != Y)
+					RSLT_Save = 0;
+				Pin9 = A;
+				if (Pin8 != Y)
 					RSLT_Save = 0;
 				Pin11 = A;
-				Pin12 = B;
-				if (Pin13 != Y)
+				if (Pin10 != Y)
+					RSLT_Save = 0;
+				Pin13 = A;
+				if (Pin12 != Y)
 					RSLT_Save = 0;
 			end
 			Done_s :
